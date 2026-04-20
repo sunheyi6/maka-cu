c725c820b2098991730d95fdffa58019ce10bfdd
Leo <ifuryst@gmail.com>
2026-04-20 17:50:01 +0800

    chore: bump open-computer-use to 0.1.17

--- apps/OpenComputerUseSmokeSuite/Sources/OpenComputerUseSmokeSuite/main.swift
+++ apps/OpenComputerUseSmokeSuite/Sources/OpenComputerUseSmokeSuite/main.swift
@@ -34,7 +34,7 @@ final class MCPClient {
         _ = try request(method: "initialize", params: [
             "clientInfo": [
                 "name": "OpenComputerUseSmokeSuite",
-                "version": "0.1.16",
+                "version": "0.1.17",
             ],
             "capabilities": [:],
             "protocolVersion": "2025-03-26",
--- packages/OpenComputerUseKit/Sources/OpenComputerUseKit/OpenComputerUseVersion.swift
+++ packages/OpenComputerUseKit/Sources/OpenComputerUseKit/OpenComputerUseVersion.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-public let openComputerUseVersion = "0.1.16"
+public let openComputerUseVersion = "0.1.17"
 
 public func resolvedOpenComputerUseVersion(bundle: Bundle = .main) -> String {
     if let version = bundle.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String,
